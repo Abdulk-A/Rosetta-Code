81a90382fd5113124fee8f777a9dc85114dbc6cb
Abdulk-A <abdullahabdulkareem273@gmail.com>
2024-04-27 15:10:14 -0500

    added swift for 100 doors

--- 100_Doors/main.swift
+++ 100_Doors/main.swift
@@ -1,15 +1,22 @@
+//swift in xcode playground
 
-var doors = Array(repeating: false, count: 100)
+let n: Int = 100
+var doors = Array(repeating: false, count: n)
 
-// for i in 0...doors.count-1 {    
-//     for j in 0...doors.count-1 {
-//         doors[j]
-//     }
-// }
-
-for i in 0...doors.count-1 {
-    for j in stride(from: 0, to: doors.count-1, by: i) {
-        print(doors[j])
-        
+for i in 1...n {
+    for j in stride(from: i - 1, to: n, by: i) {
+        doors[j].toggle()
     }
+}
+
+//print
+
+for i in doors {
+    print(i)
+}
+
+//or print like this
+
+for (i, j) in doors.enumerated() {
+    print("door \(i + 1) is \(j ? "open" : "closed")")
 }
