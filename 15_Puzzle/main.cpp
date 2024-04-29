26c7c71223bb7d61f9e9cec33258406a09dad66f
Abdulk-A <abdullahabdulkareem273@gmail.com>
2024-04-29 01:29:39 -0500

    15 puzzle completed puzzle, needs input checking

--- 15_Puzzle/main.cpp
+++ 15_Puzzle/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "../helpful.h"
 
 using namespace std;
@@ -9,48 +10,51 @@ using namespace std;
 
 bool check_board(int matrix[][BOARD_SIZE]);
 void create_board(int matrix[][BOARD_SIZE]);
-// void printMatrix2(int arr[][BOARD_SIZE], int r, int c);
+vector<int> find_zero(int matrix[][BOARD_SIZE]);
 void number_array(int* arr);
-
-
+void print_possibilites(const vector<int> & possibilities);
+void updateMatrix(int matrix[][BOARD_SIZE], const vector<int> possibilities, int num);
 
 int main() {
 
+    cout << "PLEASE MAKE SURE TO ENTER A NUMBER BETWEEN 1-15 OR Q/q to QUIT" << endl;
+
     //creating board
     int matrix[BOARD_SIZE][BOARD_SIZE];
-
-    int example[BOARD_SIZE][BOARD_SIZE] = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,0}};
     create_board(matrix);
-
-    
-    bool check = check_board(matrix);
-
-
     printMatrix(matrix);
-    cout << check << endl;
+
+    if (check_board(matrix)) return 0;
+
+    vector<int> possibilities = find_zero(matrix);
+
+    string s;
+
+    do {
+        cout << "Enter one of these numbers ";
+        possibilities = find_zero(matrix);
+        print_possibilites(possibilities);
+
+        getline(cin, s);
+
+
+        if(s == "Q" || s == "q") break;
+        int num = stoi(s);
+
+        updateMatrix(matrix, possibilities, num);
+        if (check_board(matrix)) break;
+
+        printMatrix(matrix);
+        
+
+    } while (true);
+    
+    // printMatrix(matrix);
+
+    if(check_board(matrix)) cout << "SUCCESS" << endl;
+    else cout << "FAILURE" << endl;
 
     return 0;
-}
-
-void number_array(int* arr) {
-    for(int i = 0; i < 16; i++){
-        arr[i] = i;
-    }
-}
-
-//intialize board 
-void create_board(int matrix[][BOARD_SIZE]) {
-    int arr[16];
-    number_array(arr);
-    shuffle(arr, 16);
-
-    int j = 0;
-    for(int i = 0; i < BOARD_SIZE; i++){
-        for(int k = 0; k < BOARD_SIZE; k++){
-            matrix[i][k] = arr[j];
-            j++;
-        }
-    }
 }
 
 bool check_board(int matrix[][BOARD_SIZE]) {
@@ -69,6 +73,87 @@ bool check_board(int matrix[][BOARD_SIZE]) {
     }
 
     return true;
+}
+
+//intialize board 
+void create_board(int matrix[][BOARD_SIZE]) {
+    int arr[16];
+    number_array(arr);
+    shuffle(arr, 16);
+
+    int j = 0;
+    for(int i = 0; i < BOARD_SIZE; i++){
+        for(int k = 0; k < BOARD_SIZE; k++){
+            matrix[i][k] = arr[j];
+            j++;
+        }
+    }
+}
+
+
+
+vector<int> find_zero(int matrix[][BOARD_SIZE]) {
+
+    vector<int> possibilities;
+
+    for(int i = 0; i < BOARD_SIZE; i++){
+        for(int k = 0; k < BOARD_SIZE; k++){
+            if(matrix[i][k] == 0) {
+                if(i - 1 >= 0) possibilities.push_back(matrix[i - 1][k]);
+                if(i + 1 < BOARD_SIZE) possibilities.push_back(matrix[i + 1][k]);
+                if(k - 1 >= 0) possibilities.push_back(matrix[i][k - 1]);
+                if(k + 1 <  BOARD_SIZE) possibilities.push_back(matrix[i][k + 1]);
+            }
+        }
+    }
+
+    return possibilities;
+}
+
+void number_array(int* arr) {
+    for(int i = 0; i < 16; i++){
+        arr[i] = i;
+    }
+}
+
+void print_possibilites(const vector<int> & possibilities) {
+    cout << "( ";
+    for(auto i : possibilities) {
+        cout << i << " ";
+    }
+    cout << "): ";
+}
+
+void updateMatrix(int matrix[][BOARD_SIZE], const vector<int> possibilities, int num) {
+
+    bool found = false;
+    for(auto i : possibilities) {
+        if(i == num) {
+            found = true;
+            break;
+        }
+    }
+    if(!found) {
+        cout << "Not Possible" << endl;
+        return;
+    }
+
+    int nx,ny,zx,zy;
+
+    for(int i = 0; i < BOARD_SIZE; i++){
+        for(int k = 0; k < BOARD_SIZE; k++) {
+            if(matrix[i][k] == num) {
+                nx = i;
+                ny = k;
+            }
+            if(matrix[i][k] == 0) {
+                zx = i;
+                zy = k;
+            }
+        }
+    }
+
+    swap(matrix[nx][ny], matrix[zx][zy]);
 }
 
 // void printMatrix2(int arr[][4], int r, int c) {
