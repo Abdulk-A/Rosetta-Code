6891dc4c59c317b04f95e96c0ab70069122101df
Abdulk-A <abdullahabdulkareem273@gmail.com>
2024-05-04 12:38:48 -0500

    added a caesar cipher view swift/swiftui... open in xcode

--- Caesar_Cipher/main.swift
+++ Caesar_Cipher/main.swift
@@ -7,27 +7,49 @@
 
 import SwiftUI
 
-extension MainView{
+extension CaesarCipherView{
     @Observable class ViewModel {
         var cipher = ""
-        
-        func encode() -> String {
+        var encoded = ""
+        func encode() {
             if cipher.count > 0 {
-                var rand = Int.random(in: 0...25)
+                let rand = Int.random(in: 0...25)
                 for i in 0..<cipher.count {
-                    let j = cipher.index(cipher.startIndex, offsetBy: i)
-                    let c = Character(UnicodeScalar(32))
+                    let j = cipher.index(cipher.startIndex, offsetBy: i) //getting string.index of current i
                     
-                    cipher.replaceSubrange(j...j, with: c)
+                    let ch = cipher[j]
+                    
+                    
+                    if let chVal = ch.asciiValue {
+                        if chVal >= 65 && chVal <= 90 && Int(chVal) + rand > 90 {
+                            //x = 90 - ascii
+                            //rem = rand - x
+                            //65 + rem
+                            
+                            let rem = 64 + (rand - (90 - Int(chVal)))
+
+                            cipher.replaceSubrange(j...j, with: String(UnicodeScalar(UInt8(rem))))
+                            
+                            
+                        }
+                        else if chVal >= 97 && chVal <= 122 && Int(chVal) + rand > 122 {
+                            let rem = 96 + (rand - (122 - Int(chVal)))
+                            cipher.replaceSubrange(j...j, with: String(UnicodeScalar(UInt8(rem))))
+                        }
+                        else {
+                            cipher.replaceSubrange(j...j, with: String(UnicodeScalar(UInt8(chVal) + UInt8(rand))))
+                        }
+                        
+                    }
                 }
             }
-            
-            return ""
+            encoded = cipher
+            cipher = ""
         }
     }
 }
 
-struct MainView: View {
+struct CaesarCipherView: View {
     
     @State private var vm = ViewModel()
     
@@ -36,18 +58,28 @@ struct MainView: View {
             
             HStack {
                 TextField("Please enter something to encode", text: $vm.cipher)
+                    .padding(9)
+                    .background(.gray.opacity(0.5))
+                    .clipShape(.capsule)
                 
-                Button("Encode") {vm.encode()}
+                Button("Encode") {
+                    vm.encode()
+                }
                     .padding(9)
                     .background(.blue)
                     .foregroundStyle(.white)
                     .clipShape(.capsule)
+                
+
             }
+            Text(vm.encoded)
+                .font(.title)
         }
         .padding()
+        Spacer()
     }
 }
 
 #Preview {
-    MainView()
+    CaesarCipherView()
 }
